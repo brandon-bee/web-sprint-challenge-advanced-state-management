19cea5d0c5136c37983e58a7f531e7c1242098b2
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-01-21 09:01:24 -0800

    connected App to state and fetchSMurfs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, { Component } from "react";
-
+import React, { useEffect } from "react";
+import { connect } from 'react-redux';
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
-
+import { fetchSmurfs } from './actions';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
+const App = (props) => {
+  const { fetchSmurfs } = props;
+
+  useEffect(() => {
+    fetchSmurfs();
+  }, []);
+
   return (
     <div className="App">
       <Header />
-
       <main>
         <SmurfList/>
         <AddForm/>
       </main>
     </div>
   );
+};
+
+const mapStateToProps = state => {
+  return({
+    smurfs: state.smurfs
+  })
 }
 
-export default App;
-
+export default connect(mapStateToProps, { fetchSmurfs })(App);
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
 //2. Call the fetchSmurfs action when the component mounts.
